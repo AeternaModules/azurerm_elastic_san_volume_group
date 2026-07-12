dd5e6f39b6846e148b685d3a43e9ba04ecd0f106
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:26 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "elastic_san_volume_groups" {
-  description = "All elastic_san_volume_group resources"
-  value       = azurerm_elastic_san_volume_group.elastic_san_volume_groups
-}
 output "elastic_san_volume_groups_elastic_san_id" {
-  description = "List of elastic_san_id values across all elastic_san_volume_groups"
-  value       = [for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : v.elastic_san_id]
+  description = "Map of elastic_san_id values across all elastic_san_volume_groups, keyed the same as var.elastic_san_volume_groups"
+  value       = { for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : k => v.elastic_san_id }
 }
 output "elastic_san_volume_groups_encryption" {
-  description = "List of encryption values across all elastic_san_volume_groups"
-  value       = [for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : v.encryption]
+  description = "Map of encryption values across all elastic_san_volume_groups, keyed the same as var.elastic_san_volume_groups"
+  value       = { for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : k => v.encryption }
 }
 output "elastic_san_volume_groups_encryption_type" {
-  description = "List of encryption_type values across all elastic_san_volume_groups"
-  value       = [for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : v.encryption_type]
+  description = "Map of encryption_type values across all elastic_san_volume_groups, keyed the same as var.elastic_san_volume_groups"
+  value       = { for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : k => v.encryption_type }
 }
 output "elastic_san_volume_groups_identity" {
-  description = "List of identity values across all elastic_san_volume_groups"
-  value       = [for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : v.identity]
+  description = "Map of identity values across all elastic_san_volume_groups, keyed the same as var.elastic_san_volume_groups"
+  value       = { for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : k => v.identity }
 }
 output "elastic_san_volume_groups_name" {
-  description = "List of name values across all elastic_san_volume_groups"
-  value       = [for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : v.name]
+  description = "Map of name values across all elastic_san_volume_groups, keyed the same as var.elastic_san_volume_groups"
+  value       = { for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : k => v.name }
 }
 output "elastic_san_volume_groups_network_rule" {
-  description = "List of network_rule values across all elastic_san_volume_groups"
-  value       = [for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : v.network_rule]
+  description = "Map of network_rule values across all elastic_san_volume_groups, keyed the same as var.elastic_san_volume_groups"
+  value       = { for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : k => v.network_rule }
 }
 output "elastic_san_volume_groups_protocol_type" {
-  description = "List of protocol_type values across all elastic_san_volume_groups"
-  value       = [for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : v.protocol_type]
+  description = "Map of protocol_type values across all elastic_san_volume_groups, keyed the same as var.elastic_san_volume_groups"
+  value       = { for k, v in azurerm_elastic_san_volume_group.elastic_san_volume_groups : k => v.protocol_type }
 }
 
